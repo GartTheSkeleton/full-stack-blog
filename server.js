ac612ae39a8af536be4e1e41606aac9b7082f313
Gart <gmhobson@gmail.com>
2022-05-21 08:54:29 -0500

    implement user model and routes

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const path = require('path');
-const routes = require('./controllers');
+const routes = require('./controllers/');
 const exphbs = require('express-handlebars');
 const sequelize = require('./config/connection');
 const session = require('express-session');
-//const helpers = require('./utils/helpers');
-//const hbs = exphbs.create({ helpers });
+const helpers = require('./utils/helpers');
+const hbs = exphbs.create({ helpers });
 
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
@@ -22,13 +22,17 @@ const sess = {
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(session(sess));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // turn on routes
-//app.use(routes);
+app.use(routes);
 
 // turn on connection to db and server
 sequelize.sync({ force: true }).then(() => {
